30e72cfe972fd6527b7c964cca055e3d062b0928
icarostangent <icarostangent@gmail.com>
2023-07-09 06:46:36 +0000

    del knox

--- django/myapp/urls.py
+++ django/myapp/urls.py
@@ -19,7 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('knox.urls')),
     path('api/', include('api.urls')), 
     path('snippets/', include('snippets.urls')),
     #path('api-auth/', include('rest_framework.urls')),
