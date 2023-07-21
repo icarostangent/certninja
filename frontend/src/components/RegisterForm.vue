422443c33411a4639cd4792f45a13e6eba311158
icarostangent <icarostangent@gmail.com>
2023-07-21 22:12:12 +0000

    fixxxxxxxx

--- frontend/src/components/RegisterForm.vue
+++ frontend/src/components/RegisterForm.vue
@@ -70,7 +70,7 @@
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
         >
-          Sign In
+          Sign Up
         </button>
       </div>
     </form>
