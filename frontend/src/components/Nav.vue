c8ea91f7275437a98d5f32c4c087d32734427106
josh <icarostangent@gmail.com>
2024-01-27 02:44:59 +0000

    remove home

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -5,12 +5,6 @@
         <router-link to="/" class="navbar-brand" aria-current="page">Welcome</router-link>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link to="/" class="nav-link">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/blog" class="nav-link">Blog</router-link>
-            </li>
             <template v-for="item in menu.items" :key="item.ID">
               <template v-if="item.child_items !== undefined">
                 <li class="nav-item dropdown">
@@ -25,6 +19,9 @@
                 </li>
               </template>
             </template>
+            <li class="nav-item">
+              <router-link to="/blog" class="nav-link">Blog</router-link>
+            </li>
             <li class="nav-item">
               <router-link v-show="loggedin" to="/domain" class="nav-link">Domains</router-link>
             </li>
