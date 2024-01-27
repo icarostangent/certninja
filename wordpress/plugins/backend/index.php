cae881db61bd4a944eb93f9024be258ef2efd42b
josh <icarostangent@gmail.com>
2024-01-27 03:32:23 +0000

    lol

---
+++ wordpress/plugins/backend/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+*  Plugin Name:       backend
+*  Author:            icarostangent
+*  
+**/
+
+include( plugin_dir_path( __FILE__ ) . 'roles.php');
+include( plugin_dir_path( __FILE__ ) . 'admin.php');
